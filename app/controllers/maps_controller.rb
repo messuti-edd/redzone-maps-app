8c058faeea61e39b0f1ae81813c804c7561fb9e3
unknown <messuti.edd@.(none)>
2011-07-15 10:34:08 -0400

    fb user location

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,7 +3,7 @@ class MapsController < ApplicationController
 		@geoip ||= GeoIP.new("#{Rails.root}/db/GeoIP.dat")
 		remote_ip = request.remote_ip
 		@location_pais = ""
-		@user_location = current_user.location
+		@user_location = signed_in? ? current_user.location : ''
 		
 		if remote_ip != "127.0.0.1" #todo: check for other local addresses or set default value
 			location_location = @geoip.country(remote_ip)
